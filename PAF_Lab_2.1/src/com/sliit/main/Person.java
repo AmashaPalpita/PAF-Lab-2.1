1f7c7960f7e2a19d0ac5119fae20ee96fec5bbab
AmashaPalpita <amashapalpita@yahoo.com>
2021-04-18 12:05:35 +0530

    Updated Person class

--- PAF_Lab_2.1/src/com/sliit/main/Person.java
+++ PAF_Lab_2.1/src/com/sliit/main/Person.java
@@ -1,6 +1,10 @@
 package com.sliit.main;
 
 public class Person {
-  System.out.println("This is lab 2.1.");
-  System.out.println("Additional Steps: Perform other Git operations on Eclipse");
+	public static void main(String[] args) {
+ 
+		System.out.println("This is lab 2.1.");
+		System.out.println("Additional Steps: Perform other Git operations on Eclipse");
+
+	}
 }
